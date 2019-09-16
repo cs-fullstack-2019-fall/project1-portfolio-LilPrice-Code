55a61ff032e7607ee0db4007691120c5787a5e83
LilPrice-Code <littleprice002@gmail.com>
2019-09-16 16:05:33 -0500

    About page Almost done

--- index.js
+++ index.js
@@ -4,14 +4,50 @@ let drop2 = $("#b2");
 let pro = $(".Proj");
 let drop3 = $("#b3");
 let con = $(".Cont");
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
 
 
-drop1.on("click",function () {
-    abo.slideToggle();
+drop1.on("click", function () {
+    abo.fadeToggle();
+    count1++;
+    console.log(count1);
+    if (count2 !==0 && count2 % 2 !==0){
+        count2 = 0;
+        pro.slideToggle();
+    }
+    else if (count3 !==0 && count3 % 2 !==0){
+        count3 = 0;
+        con.slideToggle();
+    }
 });
+
 drop2.on("click",function () {
-    pro.slideToggle();
+     pro.fadeToggle();
+     count2++;
+     console.log(count2);
+    if (count1 !==0 && count1 % 2 !==0){
+        count1 = 0;
+        abo.slideToggle();
+    }
+    else if (count3 !==0 && count3 % 2 !==0){
+        count3 = 0;
+        con.slideToggle();
+    }
+
 });
-drop3.on("click",function () {
-    con.slideToggle();
+drop3.on("click", function () {
+    con.fadeToggle();
+    count3++;
+    console.log(count3);
+    if (count1 !==0 && count1 % 2 !==0){
+        count1 = 0;
+        abo.slideToggle();
+    }
+    else if (count2 !==0 && count2 % 2 !==0){
+        count2 = 0;
+        pro.slideToggle();
+    }
 });
+
