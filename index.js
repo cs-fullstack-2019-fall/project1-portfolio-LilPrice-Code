368eda6241976d4e24ae2a56ade2fb22b06a22ac
LilPrice-Code <littleprice002@gmail.com>
2019-09-18 09:29:12 -0500

    Contact Me in Progress

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ let drop2 = $("#b2");
 let pro = $(".Proj");
 let drop3 = $("#b3");
 let con = $(".Cont");
+let drop4 = $("#gform");
+let goggle = $("#forms");
+let drop5 = $("#bbutton");
 let count1 = 0;
 let count2 = 0;
 let count3 = 0;
+let count4 = 0;
 
 //Swap to About Me page
 drop1.on("click", function () {
@@ -56,3 +60,14 @@ drop3.on("click", function () {
     }
 });
 
+drop4.on("click", function () {
+    goggle.slideToggle();
+    count4++;
+    console.log(count4);
+});
+
+drop5.on("click", function () {
+    goggle.slideToggle();
+    count4++;
+    console.log(count4);
+});
